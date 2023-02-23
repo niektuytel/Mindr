26175141eefd00a46d2e7883898627bad06d25de
niektuytel <46424968+niektuytel@users.noreply.github.com>
2023-02-24 00:12:04 +0100

    Create connector page #1

---
+++ src/Call.Mailing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Call.Mailing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ src/Call.WhatsApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Call.WhatsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
