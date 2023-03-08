d5ed90f6729a2cdadb6d644ba431e1af3f086334
niektuytel <46424968+niektuytel@users.noreply.github.com>
2023-03-08 15:08:21 +0100

    Init Mindr.Api

---
+++ src/Mindr.Api/Controllers/CollectionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace Mindr.Api.Controllers;
+
+//[Authorize]
+//[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+[ApiController]
+[Route("[controller]")]
+public class ConnectorController : ControllerBase
+{
+
+}
