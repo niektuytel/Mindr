3f18a93d13edadf10ad8ead95eeece1a7cdae4c9
niektuytel <46424968+niektuytel@users.noreply.github.com>
2023-05-23 00:55:37 +0200

    Finish ConnectorView

--- src/Mindr.WebAssembly.Client/Pages/Connectors/Views/ConnectorOverview.razor.cs
+++ src/Mindr.WebAssembly.Client/Pages/Connectors/Views/ConnectorOverview.razor.cs
@@ -5,10 +5,11 @@ using Mindr.WebAssembly.Client.Pages.Connectors.Components;
 using Mindr.WebAssembly.Client.Services;
 using Mindr.Domain.Models.DTO.Connector;
 using System.Threading.Tasks;
+using MudBlazor;
 
 namespace Mindr.WebAssembly.Client.Pages.Connectors.Views;
 
-public partial class ConnectorOverview : FluentComponentBase
+public partial class ConnectorOverview
 {
     [Inject]
     public NavigationManager NavigationManager { get; set; }
@@ -16,10 +17,12 @@ public partial class ConnectorOverview : FluentComponentBase
     [Inject]
     public IApiConnectorClient ConnectorClient { get; set; }
 
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = default!;
+
     [Parameter, EditorRequired]
     public ConnectorOverviewDTO Overview { get; set; } = null!;
 
-    private string? ErrorMessage { get; set; }
     private bool DataHasChanged = false;
 
     public ConfirmDialog RemoveItemDialog = default!;
@@ -27,8 +30,13 @@ public partial class ConnectorOverview : FluentComponentBase
     public async Task OnSave()
     {
         var response = await ConnectorClient.UpdateOverview(Overview);
-        (_, ErrorMessage) = response.AsTuple();
-        base.StateHasChanged();
+        (_, var error) = response.AsTuple();
+
+        if (!string.IsNullOrEmpty(error))
+        {
+            Snackbar.Add(error, Severity.Error);
+            base.StateHasChanged();
+        }
     }
 
     public async Task OnRemove()
